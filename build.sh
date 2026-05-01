6c115b9c0ff9fd1a56170f6664675697a61558a4
JamshidJalolov <jamshidjalolov6767@gmail.com>
2026-05-01 21:02:52 +0500

    Fix Poetry build path - run from backend directory

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-# Copy backend config to root for Render to find
-cp backend/pyproject.toml .
-
 # Install dependencies for backend
 cd backend
 poetry install --no-root
+cd ..
